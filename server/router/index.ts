a99527181b766bc04a32cda5445bebdff68d66c9
BetaHuhn <schiller@mxis.ch>
2021-07-04 16:30:56 +0200

    Wildcard add

--- server/router/index.ts
+++ server/router/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 
 import apiRouter from './api'
 import redirectRouter from './redirect'
+import wildcardAddRouter from './wildcardAdd'
 
 export const router = express.Router()
 
 router.use('/api', apiRouter)
 router.use('/r', redirectRouter)
+router.use('/n', wildcardAddRouter)
 
 router.get('/test', (_req: express.Request, res: express.Response) => {
 	res.send('Hello World')
