92e35ac78af0740e4835ea40095eff3b26af2a71
BetaHuhn <schiller@mxis.ch>
2021-07-09 20:04:24 +0200

    Use Deta SDK's own types

--- server/types.ts
+++ server/types.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-namespace */
 declare module 'nuxt'
-declare module 'deta'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 declare namespace Express {
