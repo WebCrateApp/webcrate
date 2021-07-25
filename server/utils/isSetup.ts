1da2c17308f99a86d02d3bab9687415303605ee4
BetaHuhn <schiller@mxis.ch>
2021-07-25 14:45:07 +0200

    Use config to check if setup

--- server/utils/isSetup.ts
+++ server/utils/isSetup.ts
@@ -1,12 +1,9 @@
-import { Crate } from '../models/crate'
-import { Link } from '../models/link'
+import { Config } from '../models/config'
 
 export const isSetup = async () => {
-	const links = await Link.find({}, 1)
-	const crates = await Crate.find({}, 3)
+	const config = await Config.get()
 
-	// Assume not setup when no links exist and we have fewer than 3 crates (2 are created during welcome)
-	if (links.length === 0 && crates.length < 3) {
+	if (!config || !config.name) {
 		return false
 	}
 
