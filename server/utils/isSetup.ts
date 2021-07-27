9ff8219177cdcb894601416367c4ae5db74640ca
BetaHuhn <schiller@mxis.ch>
2021-07-27 14:58:18 +0200

    Override isSetup with env var

--- server/utils/isSetup.ts
+++ server/utils/isSetup.ts
@@ -1,6 +1,12 @@
 import { Config } from '../models/config'
 
 export const isSetup = async () => {
+	const env = process.env.OVERWRITE_IS_SETUP
+
+	if (env !== undefined) {
+		return env === 'true'
+	}
+
 	const config = await Config.get()
 
 	if (!config || !config.name) {
