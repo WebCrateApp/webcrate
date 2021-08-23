584fd403b428e5fca601225b3abd76c37129b062
BetaHuhn <schiller@mxis.ch>
2021-08-23 20:43:31 +0200

    🚨 Update lint script and fix lint errors

--- server/utils/random.ts
+++ server/utils/random.ts
@@ -3,7 +3,7 @@ import { nanoid, customAlphabet } from 'nanoid'
 // Used as large number to make sure keys are generated in descending order
 const maxDateNowValue = 8.64e15 // Fun fact: This will only work until the year 275760
 
-export const generateKey = (descending: Boolean) => {
+export const generateKey = (descending: boolean) => {
 	const id = nanoid(10)
 	const timestamp = descending ? maxDateNowValue - Date.now() : Date.now()
 
