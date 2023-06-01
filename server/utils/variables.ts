80002b5c6c7e13a96dcd372ba563360ecb23c136
BetaHuhn <hello@mxis.ch>
2023-06-01 19:19:35 +0200

    small fix

--- server/utils/variables.ts
+++ server/utils/variables.ts
@@ -1,6 +1,6 @@
 export const isDev = process.env.DETA_RUNTIME !== 'True' && process.env.DETA_RUNTIME !== 'true' && process.env.DETA_SPACE_APP !== 'true'
 export const isSpace = process.env.DETA_SPACE_APP === 'True' || process.env.DETA_SPACE_APP === 'true'
-export const subdomain = process.env.DETA_SPACE_APP_HOSTNAME
-export const domain = isDev ? 'localhost:3000' : `${ subdomain }.${ isSpace ? 'deta.app' : 'deta.dev' }`
+// eslint-disable-next-line no-nested-ternary
+export const domain = isDev ? 'localhost:3000' : isSpace ? process.env.DETA_SPACE_APP_HOSTNAME : `${ process.env.DETA_SPACE_APP_HOSTNAME }.deta.dev`
 export const version = process.env.DETA_SPACE_APP_VERSION
-export const latestReleaseUrl = process.env.LATEST_RELEASE_URL || 'https://alpha.deta.space/api/v0/discovery/apps/@maxs1/webcrate'
+export const latestReleaseUrl = process.env.LATEST_RELEASE_URL || 'https://deta.space/api/v0/discovery/apps/@maxs1/webcrate'
